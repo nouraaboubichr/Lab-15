86dfebe65784bdf701f8e24e1ce915adc33927a9
nouraaboubichr <nouraaboubichr@gmail.com>
2026-09-16 00:08:23 +0100

    Update Validateur.java

--- src/entities/Validateur.java
+++ src/entities/Validateur.java
@@ -5,10 +5,49 @@
  */
 package entities;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 /**
  *
  * @author hp
  */
-public class Validateur {
-    
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Validateur<T> {
+
+    private final List<Regle<T>> regles = new ArrayList<>();
+
+    public Validateur<T> ajouterRegle(Predicate<T> test, String messageErreur) {
+        regles.add(new Regle<>(test, messageErreur));
+        return this;
+    }
+
+    public List<String> valider(T valeur) {
+
+        List<String> erreurs = new ArrayList<>();
+
+        for (Regle<T> regle : regles) {
+            if (!regle.test.test(valeur)) {
+                erreurs.add(regle.messageErreur);
+            }
+        }
+
+        return erreurs;
+    }
+
+    private static class Regle<T> {
+        final Predicate<T> test;
+        final String messageErreur;
+
+        Regle(Predicate<T> test, String messageErreur) {
+            this.test = test;
+            this.messageErreur = messageErreur;
+        }
+    }
 }
